bd882d811839b6312ecb45e024af789a703f0ff1
joyoung <githuborigin@gmail.com>
2024-08-28 17:04:13 +0900

    fix : 알럿창으로 수정

--- src/pages/BeforeLeaveExample/BeforeLeaveExample.tsx
+++ src/pages/BeforeLeaveExample/BeforeLeaveExample.tsx
@@ -4,13 +4,14 @@ import HeadingFont from "@/component/typography/HeadingFont";
 import { useBeforeLeave } from "@/hooks/useBeforeLeave";
 
 export default function BeforeLeaveExample() {
-  const beforeLife = () => console.log("pls don't leave");
+  const beforeLife = () => alert("나갈거야?");
   useBeforeLeave(beforeLife);
 
   return (
     <MainLayout>
       <HeadingFont> useBeforeLeave</HeadingFont>
-      <BodyFont>페이지를 닫을 떄 실행 되는 함수</BodyFont>
+      <BodyFont>페이지를 벗어 날 때 실행 되는 함수</BodyFont>
+      <BodyFont>* 브라우저 밖으로 마우스를 꺼내면 알럿 창이 뜹니다</BodyFont>
     </MainLayout>
   );
 }
