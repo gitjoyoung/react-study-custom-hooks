c0af6c3c840ae6f5265bcff7b3c84b503c837d50
joyoung <githuborigin@gmail.com>
2024-08-30 14:41:31 +0900

    feat: 이미지 동적 좌표 처리

--- src/pages/ImageHookExample/ImageHookExample.tsx
+++ src/pages/ImageHookExample/ImageHookExample.tsx
@@ -3,138 +3,94 @@ import BodyFont from "@/component/typography/BodyFont";
 import HeadingFont from "@/component/typography/HeadingFont";
 import * as d3 from "d3";
 import { useEffect, useRef, useState } from "react";
+import { boxCoordinatesArray } from "./data";
 
-// Custom hook to handle image labeling with bounding box
-type BoxCoordinates = {
-  IMG_LEFT: number;
-  IMG_TOP: number;
-  IMG_WIDTH: number;
-  IMG_HEIGHT: number;
-};
+type BoxCoordinates = Record<
+  "IMG_LEFT" | "IMG_TOP" | "IMG_WIDTH" | "IMG_HEIGHT",
+  number
+>;
 
 const useImageLabel = (src: string, boxCoordinates: Array<BoxCoordinates>) => {
   const [visible, setVisible] = useState(true);
   const handleToggle = () => {
     setVisible(!visible);
   };
-
   const svgRef = useRef<SVGSVGElement>(null);
-  const containerRef = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<HTMLImageElement>(null);
 
   useEffect(() => {
-    const svg = d3.select(svgRef.current);
-    svg.selectAll("*").remove();
+    const svg = d3.select(svgRef.current); // svg 선택
 
-    if (!visible) return;
+    if (!visible || !containerRef.current) return; // visible 이 false 이거나 container 가 없으면 return
 
-    const img = new Image();
-    img.src = src;
-    img.onload = () => {
-      const imgWidth = img.width;
-      const imgHeight = img.height;
+    const updateSVG = () => {
+      svg.selectAll("*").remove(); // svg 초기화
+      const containerWidth = containerRef.current?.clientWidth || 0; // container 의 width
+      const containerHeight = containerRef.current?.clientHeight || 0; // container 의 height
 
-      const containerWidth = containerRef.current?.clientWidth || 0;
-      const containerHeight = containerRef.current?.clientHeight || 0;
+      const img = new Image(); // 이미지 객체 생성
+      img.src = src;
+      img.onload = () => {
+        // 이미지 로드가 완료되면 실행
+        svg.attr("width", containerWidth).attr("height", containerHeight); // svg 의 width, height 설정
 
-      const scaleX = containerWidth / imgWidth;
-      const scaleY = containerHeight / imgHeight;
+        boxCoordinates.forEach((item) => {
+          const { IMG_LEFT, IMG_TOP, IMG_WIDTH, IMG_HEIGHT } = item; // 좌표값 데이터
 
-      svg.attr("width", containerWidth).attr("height", containerHeight);
+          const adjustedLeft = (IMG_LEFT / img.width) * containerWidth; //
+          const adjustedTop = (IMG_TOP / img.height) * containerHeight;
+          const adjustedWidth = (IMG_WIDTH / img.width) * containerWidth;
+          const adjustedHeight = (IMG_HEIGHT / img.height) * containerHeight;
 
-      boxCoordinates.forEach((item) => {
-        const { IMG_LEFT, IMG_TOP, IMG_WIDTH, IMG_HEIGHT } = item;
-        const text = "dataMatrix";
+          const group = svg.append("g");
 
-        const adjustedLeft = IMG_LEFT * scaleX;
-        const adjustedTop = IMG_TOP * scaleY;
-        const adjustedWidth = IMG_WIDTH * scaleX;
-        const adjustedHeight = IMG_HEIGHT * scaleY;
+          group
+            .append("rect")
+            .attr("x", adjustedLeft)
+            .attr("y", adjustedTop)
+            .attr("width", adjustedWidth)
+            .attr("height", adjustedHeight)
+            .attr("stroke", "red")
+            .attr("stroke-width", 2)
+            .attr("fill", "none");
 
-        svg
-          .append("rect")
-          .attr("x", adjustedLeft)
-          .attr("y", adjustedTop)
-          .attr("width", adjustedWidth)
-          .attr("height", adjustedHeight)
-          .attr("stroke", "red")
-          .attr("stroke-width", 2)
-          .attr("fill", "none");
+          group
+            .append("rect")
+            .attr("x", adjustedLeft + adjustedWidth)
+            .attr("y", adjustedTop)
+            .attr("width", 100)
+            .attr("height", adjustedHeight)
+            .attr("fill", "red")
+            .attr("stroke-width", 2)
+            .attr("stroke", "red");
 
-        svg
-          .append("rect")
-          .attr("x", adjustedLeft + adjustedWidth)
-          .attr("y", adjustedTop)
-          .attr("width", 100)
-          .attr("height", adjustedHeight)
-          .attr("fill", "red")
-          .attr("stroke-width", 2)
-          .attr("stroke", "red");
+          group
+            .append("text")
+            .attr("x", adjustedLeft + adjustedWidth + 5)
+            .attr("y", adjustedTop + adjustedHeight / 2)
+            .attr("fill", "black")
+            .text("text")
+            .style("alignment-baseline", "middle");
+        });
+      };
+    };
 
-        svg
-          .append("text")
-          .attr("x", adjustedLeft + adjustedWidth + 5)
-          .attr("y", adjustedTop + adjustedHeight / 2)
-          .attr("fill", "black")
-          .text(text)
-          .style("alignment-baseline", "middle");
-      });
+    const resizeObserver = new ResizeObserver(() => {
+      updateSVG();
+    });
+
+    resizeObserver.observe(containerRef.current);
+
+    updateSVG();
+
+    return () => {
+      resizeObserver.disconnect();
     };
   }, [src, boxCoordinates, visible]);
-
   return { svgRef, containerRef, visible, handleToggle };
 };
 
 export default function ImageHookExample() {
-  const boxCoordinatesArray = [
-    {
-      IMG_LEFT: 2158,
-      IMG_TOP: 4424,
-      IMG_WIDTH: 77,
-      IMG_HEIGHT: 77,
-    },
-    {
-      IMG_LEFT: 2436,
-      IMG_TOP: 1519,
-      IMG_WIDTH: 79,
-      IMG_HEIGHT: 79,
-    },
-    {
-      IMG_LEFT: 3497,
-      IMG_TOP: 1492,
-      IMG_WIDTH: 87,
-      IMG_HEIGHT: 88,
-    },
-    {
-      IMG_LEFT: 4654,
-      IMG_TOP: 1436,
-      IMG_WIDTH: 77,
-      IMG_HEIGHT: 76,
-    },
-    {
-      IMG_LEFT: 3106,
-      IMG_TOP: 4558,
-      IMG_WIDTH: 74,
-      IMG_HEIGHT: 73,
-    },
-    {
-      IMG_LEFT: 4435,
-      IMG_TOP: 3377,
-      IMG_WIDTH: 74,
-      IMG_HEIGHT: 75,
-    },
-    {
-      IMG_LEFT: 4482,
-      IMG_TOP: 4110,
-      IMG_WIDTH: 96,
-      IMG_HEIGHT: 97,
-    },
-    {
-      IMG_LEFT: 4019,
-      IMG_TOP: 2792,
-      IMG_WIDTH: 72,
-      IMG_HEIGHT: 72,
-    },
-  ];
   const imageUrl = "images/dataMatrix3.png";
   const { svgRef, containerRef, handleToggle, visible } = useImageLabel(
     imageUrl,
@@ -157,11 +113,9 @@ export default function ImageHookExample() {
         />
         <BodyFont>{visible ? "라벨을 표시합니다" : "라벨을 숨깁니다"}</BodyFont>
       </div>
-      <div
-        ref={containerRef}
-        className="relative w-full h-full my-10  border-red-200"
-      >
+      <div className="relative w-full h-full my-10  border-red-200">
         <img
+          ref={containerRef}
           src={imageUrl}
           className="w-full h-auto object-contain bg-no-repeat "
         />
