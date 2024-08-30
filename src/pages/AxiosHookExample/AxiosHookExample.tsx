447da883fcfcee7ea3437804b5cb0cd3c7c5c9f5
joyoung <githuborigin@gmail.com>
2024-08-30 16:51:08 +0900

    feat: Axios 훅 예제 컴포넌트 생성

---
+++ src/pages/AxiosHookExample/AxiosHookExample.tsx
@@ -0,0 +1,26 @@
+import PrimaryButton from "@/component/Button";
+import MainLayout from "@/component/LayOut/MainLayout";
+import BodyFont from "@/component/typography/BodyFont";
+import HeadingFont from "@/component/typography/HeadingFont";
+import { useAxios } from "@/hooks/useAxios";
+
+export default function AxiosHookExample() {
+  const { loading, data, error, refetch } = useAxios({
+    url: "https://jsonplaceholder.typicode.com/posts",
+  });
+  console.log(`loading : ${loading}  data : ${data} error : ${error}`);
+  return (
+    <MainLayout>
+      <HeadingFont>useAxios</HeadingFont>
+      <BodyFont>axios 통신 훅</BodyFont>
+      <div className="flex justify-center items-center">
+        {loading ? (
+          <BodyFont>로딩중...</BodyFont>
+        ) : (
+          <PrimaryButton onClick={refetch}>다시 불러오기</PrimaryButton>
+        )}
+      </div>
+      <BodyFont> {data && JSON.stringify(data)} </BodyFont>
+    </MainLayout>
+  );
+}
