c90f9530c97382273a0fa04b82e4cab52a4fe29a
joyoung <githuborigin@gmail.com>
2024-08-30 16:50:47 +0900

    feat: useAxios 훅 생성

---
+++ src/hooks/useAxios.ts
@@ -0,0 +1,48 @@
+import defaultAxios from "axios";
+import { useEffect, useState } from "react";
+
+interface IOptions {
+  url?: string;
+  method?: "get" | "post";
+}
+
+interface IState {
+  loading?: boolean;
+  data?: any;
+  error?: any;
+  refetch?: () => void;
+}
+export const useAxios = (
+  opts: IOptions,
+  axiosInstance = defaultAxios
+): IState => {
+  const [state, setState] = useState<IState>({
+    loading: true,
+    data: null,
+    error: null,
+  });
+
+  const [trigger, setTrigger] = useState(0);
+  const refetch = () => {
+    setState({
+      ...state,
+      loading: true,
+    });
+    setTrigger(Date.now());
+  };
+
+  if (!opts.url) {
+    return state;
+  }
+  useEffect(() => {
+    axiosInstance(opts)
+      .then((data) => {
+        setState({ ...state, loading: false, data });
+      })
+      .catch((error) => {
+        setState({ ...state, loading: false, error });
+      });
+  }, [trigger]);
+
+  return { ...state, refetch };
+};
